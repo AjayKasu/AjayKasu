e5650fbd5dee72f5ac05f6f22cdda866f5453aa3
Ajay Kasu <95525485+AjayKasu@users.noreply.github.com>
2022-12-04 00:12:49 +0530

    Java Journey
    Basics java programming.

---
+++ Java Journey.java
@@ -0,0 +1,33 @@
+// I start my journey with java "ika medala edadama">> JAVA by JAMES GOSLING in 1995. 
+class JavaJourney// According to real-time coding standards the names must be started with UpperCase Letters.
+{
+  public static void main(String args[])
+  {
+    System.out.println(" Welcome to my JAVA world");
+  }
+}  
+/*In java has Five-modules i.e. 1.programming module(main one)>> core java>> Advance java>> Spring>> Web services.
+2.UI(user-interface) module>> HTML/CSS/JS>>Anguler. 3.Testing Module>> Testing basics>> selenium 
+4.Devsecoops(Development+Security+Operations)5. Database module>>Oracle>>Mysql*/
+// User deatials 
+class UserDetails
+{
+  public static void main(String args[])
+  {
+    String userName="AJAY";// a group of characters is String.
+    String mailId="ajayyadavKasu00@gmail.com";
+    long phnNo=7569900932L;// primitive datatype
+    float x=(float)phnNo;// type conversion
+    boolean b= true;
+    char ch='A';// A single character.
+    System.out.println("username:"+userName);
+    System.out.println("mailid:"+mailId);
+    System.out.println("phnNo:"+phnNo);
+    System.out.println("x:"+x+"\n"+b+"\n"+ch);
+  }
+} 
+ 
+   
+    
+    
+    
